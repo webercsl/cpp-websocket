15627896df7896af59bb4fd94941e82e462bdd34
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2023-09-14 23:03:05 -0300

    starting the struct creation

--- socketUtils.h
+++ socketUtils.h
@@ -6,6 +6,41 @@
 #include <netinet/in.h>
 #include <string.h>
 
+#define REQUEST_BUFFER_SIZE = 174
+
+enum op_codes{
+    ADD = 0,
+    LIST = 1
+};
+
+struct request_t{
+    op_codes op;
+    char name[50], content[100], date[19];
+};
+
+const char * requestToStr(request_t * request){
+    if(request == NULL)
+        return "";
+
+    std::string requestText = "";
+    requestText += std::to_string((int)request->op);
+    requestText += std::string(";");
+    
+    requestText += std::string(request->name);
+    requestText += std::string(";");
+
+    requestText += std::string(request->content);
+    requestText += std::string(";");
+
+    requestText += std::string(request->date);
+    requestText += std::string(";");
+
+    return requestText.c_str();
+}
+
+request_t * strToRequest(char * str){
+
+}
 
 void pressAnyKeyToContinue(){
     std::cout << std::endl << "Pressione qualquer tecla para continuar..";
