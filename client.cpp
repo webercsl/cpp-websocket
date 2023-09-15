6e101eadfb559225b77d30863183921c558fc99a
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2023-09-14 21:06:43 -0300

    basic menu for client

--- client.cpp
+++ client.cpp
@@ -1,5 +1,38 @@
 #include "client.h"
 
 int main(int argc, char *argv[]){
+    int returnCode = 0;
+    int userInput;
+
+    while (true)
+    {
+        std::cout << "Menu:" << std::endl << std::endl;
+        std::cout << "[1] - Enviar arquivo para impressão" << std::endl;
+        std::cout << "[2] - Ver fila de impressão" << std::endl;
+        std::cout << "[0] - Sair" << std::endl;
+        
+        std::cin >> userInput;
+        system("clear");
+        switch(userInput){
+            case 0:
+                goto leave;
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            default:
+                std::cout << "Digite uma entrada válida!" << std::endl;
+                continue;
+        }
+        pressAnyKeyToContinue();
+        system("clear");
+    }
     
+
+
+leave:
+    return returnCode;
 }
